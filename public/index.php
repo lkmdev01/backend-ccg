c87abdaa867daf63819c2e91bc40205d98ddbdd1
lkmdev01 <lkmdev01@gmail.com>
2024-11-13 19:13:37 -0300

    Alterar

--- public/index.php
+++ public/index.php
@@ -7,9 +7,5 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 
-// Carrega as variáveis de ambiente
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
-
 // Inclui o arquivo de rotas
 require_once __DIR__ . '/../src/routes.php';
